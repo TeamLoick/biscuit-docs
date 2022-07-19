a7677b5c82d8dffcab1bb18ee16d2160a787ba84
Dragurimu <Drakgons7OfDragon@hotmail.com>
2022-07-18 19:49:20 -0500

    docs

---
+++ docs/_app/immutable/chunks/docs-a936db8f.js
@@ -0,0 +1,1 @@
+function s(e){let l="https://github.com/oasisjs/biscuit/tree/main",t=e.filename,n=t.indexOf("/packages/"),i=t.slice(n,t.length),a=`#L${e.line}:${e.col}`;return l+i+a}function r(e){return e.filename.split("/")[9]}export{s as l,r as p};
